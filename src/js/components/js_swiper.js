eabd8d0d3136fc23d0f6d782581567f1c17f296a
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-07-28 22:57:03 +0300

    start project

---
+++ src/js/components/js_swiper.js
@@ -0,0 +1,39 @@
+import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
+
+export default function () {
+    const slider = '.js_swiper';
+
+    const swiper = new Swiper(slider, {
+        modules: [Autoplay],
+        autoplay: {
+            delay: 3000
+            // disableOnInteraction: false,
+        },
+        loop: true,
+        speed: 500
+    });
+
+    swiper.autoplay.stop();
+
+    const $controls = document.querySelectorAll('[data-control]');
+
+    if ($controls.length) {
+        $controls.forEach($control => {
+            $control.addEventListener('click', e => {
+                e.preventDefault();
+                e.stopPropagation();
+
+                if ($control.dataset.control === 'next') {
+                    swiper.slideNext();
+                } else {
+                    swiper.slidePrev();
+                }
+            });
+        });
+    }
+
+    setTimeout(function () {
+        swiper.autoplay.start();
+    }, 1000);
+}
---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+// import js_swiper from './components/js_swiper.js';
+
+(function () {
+    // js_swiper();
+})();
